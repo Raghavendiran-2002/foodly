d2bf8be3d7518e38b58258292462fa240bc96778
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2022-11-12 09:36:07 +0530

    when user's add create new doc

--- Test-Deploy/NewUser-DocumentCreate/functions/index.js
+++ Test-Deploy/NewUser-DocumentCreate/functions/index.js
@@ -1,16 +1,15 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const {Firestore} = require("@google-cloud/firestore");
 
-admin.initializeApp();
+const firestore = new Firestore();
 
 exports.sendByeEmail = functions.auth.user().onCreate(async (user) => {
   console.log("Running The Function");
-  const writeResult = await admin
-    .firestore()
-    .collection("usersCollection")
-    .add({UID: user.uid, phone: user.email, createOn: user.createOn});
-
-  return writeResult.then(() => {
-    console.log("Succesfully Created Document");
-  });
+  const data = {UID: user.uid, email: user.email};
+  await firestore.collection("usersCollection").add(data);
+  console.log("Completed DB Upload");
+  return "hI";
+  // return writeResult.then(() => {
+  //   console.log("Succesfully Created Document");
+  // });
 });
