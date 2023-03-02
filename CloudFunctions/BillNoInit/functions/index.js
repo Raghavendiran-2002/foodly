745b711175ce7a687357b55642af44748ab858fc
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-03-02 17:13:39 +0530

    generateBillNumber with transaction

--- CloudFunctions/BillNoInit/functions/index.js
+++ CloudFunctions/BillNoInit/functions/index.js
@@ -9,32 +9,47 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-var tokenNo = 1;
-exports.billNumber = functions.firestore
+
+exports.generateBillNumber = functions.firestore
   .document("vendors/{U6FKmkY682MEy8LlDIiX}/activeOrders/{qwe}")
-  .onCreate((snap, context) => {
-    const newValue = snap.data();
-    console.log("*****************************");
-    console.log(snap.id);
-    var data = {
-      tokenNo: `${tokenNo}`,
-    };
-    db.collection("vendors")
-      .doc("U6FKmkY682MEy8LlDIiX")
-      .collection("activeOrders")
-      .doc(`${snap.id}`)
-      .set(data, { merge: true })
-      .then((v) => {
-        console.log(v);
-        console.log("Added Token");
+  .onCreate(async (snap, context) => {
+    const getOrderNo = db.collection("vendors").doc("U6FKmkY682MEy8LlDIiX");
+    try {
+      await db.runTransaction(async (transaction) => {
+        const doc = await transaction.get(getOrderNo);
+
+        const orderNo = doc.data().currentOrderCounter + 1;
+        var data = {
+          tokenNo: `${orderNo}`,
+        };
+        db.collection("vendors")
+          .doc("U6FKmkY682MEy8LlDIiX")
+          .collection("activeOrders")
+          .doc(`${snap.id}`)
+          .set(data, { merge: true })
+          .then((v) => {
+            console.log("Added Token");
+          });
+        transaction.update(getOrderNo, { currentOrderCounter: orderNo });
+        console.log(`Token Added ${orderNo}`);
       });
-    tokenNo++;
+      console.log("Transaction success!");
+    } catch (e) {
+      console.log("Transaction failure:", e);
+    }
     return null;
   });
 
-exports.moveExpiredDocuments = functions.pubsub
-  .schedule("0 0 * * *") // https://crontab.guru/#*_*_*_*_*
-  .onRun(async (context) => {
-    tokenNo = 1;
-    return null;
-  });
+// exports.billingStatus = functions.firestore
+//   .document("vendors/{U6FKmkY682MEy8LlDIiX}/activeOrders/{qwe}")
+//   .onUpdate((change, context) => {
+//     // const newValue = change.after.data();
+//     // const previousValue = change.before.data();
+//   });
+
+// exports.moveExpiredDocuments = functions.pubsub
+//   .schedule("0 0 * * *") // https://crontab.guru/#*_*_*_*_*
+//   .onRun(async (context) => {
+//     tokenNo = 1;
+//     return null;
+//   });
